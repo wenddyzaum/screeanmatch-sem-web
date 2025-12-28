8f26876eedab8978adf59e6cd8aa666c8ad37427
gabriela <mariagabrielapereiradasilva2@gmail.com>
2025-12-28 20:13:05 -0300

    codigo funcionando devidamente

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.alura.screenmatch.service;
 
 import java.io.IOException;
@@ -28,34 +27,3 @@ public class ConsumoApi {
         return json;
     }
 }
-=======
-package br.com.alura.screenmatch.service;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-public class ConsumoApi {
-
-    public String obterDados(String endereco) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = null;
-        try {
-            response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        String json = response.body();
-        return json;
-    }
-}
->>>>>>> b6f17c5e04ef4af1b4bae388635e6f439c63d286
--- screenmatch/src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package br.com.alura.screenmatch.service;
 
 public interface IConverteDados {
    <T> T obterDados( String json, Class<T> classe);
 }
-=======
-package br.com.alura.screenmatch.service;
-
-public interface IConverteDados {
-   <T> T obterDados( String json, Class<T> classe);
-}
->>>>>>> b6f17c5e04ef4af1b4bae388635e6f439c63d286
